1d4eb95d2e011571a45428513207957414b45ff5
naokirin <naoki.rin186@gmail.com>
2025-05-26 01:22:37 +0900

    Add jest-html-reporters

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,14 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "js", "json"],
   testMatch: ["**/__tests__/**/*.test.ts", "**/*.test.ts"],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        outputPath: "jest_html_reporters.html",
+        includeConsoleLog: true,
+      },
+    ]
+  ]
 };
